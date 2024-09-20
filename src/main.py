ec3b366d3be22bafc98ccb71914fb8b07b27e3a3
khaledelhady44 <khaledelhady44@gmail.com>
2024-09-20 10:47:39 +0300

    Start a connection to create my database and update config to hold database url and name.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 from helpers.config import get_settings
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_settings()
+
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    app.db_client["coll"].insert_one({"key": "value"})
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
