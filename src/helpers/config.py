b9fb4c15077a8f0e7c9ecf1e098ccd1be1110739
khaledelhady44 <khaledelhady44@gmail.com>
2024-09-25 09:21:32 +0300

    create auth helpers.

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,9 +6,13 @@ class Settings(BaseSettings):
 
     MONGODB_URL: str
     MONGODB_DATABASE: str
+    
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
-        env_file = "/mnt/d/The-boy-who-lived/src/.env"
+        env_file = "/mnt/d/The-boy-who-lived/src/.env" # path problem
 
 
 def get_settings():
