ba8bff664fb6d72a42e146e3b7375da5a817c3a2
khaledelhady44 <khaledelhady44@gmail.com>
2024-10-12 11:42:43 +0300

    Change the default value of sender in message schema.

--- src/schemas/message/base_message.py
+++ src/schemas/message/base_message.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, Field
 from enums import ChatSender
+from typing import Literal
 from datetime import datetime
 
 class BaseMessage(BaseModel):
     chat_id: str = Field(...)
-    sender: ChatSender
+    sender: Literal[ChatSender.SYSTEM.value, ChatSender.USER.value] # type: ignore
     message: str = Field(..., min_length = 1)
     timestamp : datetime = Field(default_factory=datetime.utcnow)
+
