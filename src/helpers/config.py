5aab481b0df732c79bbae9daf08b82d329cd5d55
khaledelhady44 <khaledelhady44@gmail.com>
2024-11-26 13:08:37 +0200

    add google api key to config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -35,6 +35,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    GOOGLE_API_KEY: str
+
     class Config:
         env_file = ".env"  # Relative path from the script's location
 
