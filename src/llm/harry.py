3ecd01c95e7e5c58a156a33dfd028259a83f88ae
khaledelhady44 <khaledelhady44@gmail.com>
2024-11-26 15:26:30 +0200

    add  mongodb saver to use as a checkpointer  and use it in generating responses.

--- src/llm/harry.py
+++ src/llm/harry.py
@@ -1,13 +1,13 @@
 import os
 from langchain.schema import AIMessage
-from langgraph.checkpoint.memory import MemorySaver
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph import START, MessagesState, StateGraph, END
 from helpers import get_settings
 from llm.mongo_db_saver import MongoDBSaver
 from llm.prompts import harry_prompt
 
-os.environ["GOOGLE_API_KEY"] =  get_settings().GOOGLE_API_KEY
+settings = get_settings()
+os.environ["GOOGLE_API_KEY"] =  settings.GOOGLE_API_KEY
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
     temperature=0.7,
@@ -27,33 +27,28 @@ def call_llm(state: MessagesState):
 
 
 
-workflow = StateGraph(state_schema=MessagesState)
+def create_graph(checkpointer):
 
-workflow.add_node("harry", call_llm)
-workflow.add_edge(START, "harry")
-workflow.add_edge("harry", END)
+    workflow = StateGraph(state_schema=MessagesState)
+    workflow.add_node("harry", call_llm)
+    workflow.add_edge(START, "harry")
+    workflow.add_edge("harry", END)
 
-
-memory = MemorySaver()
-app = workflow.compile(checkpointer=memory)
+    app = workflow.compile(checkpointer=checkpointer)
+    return app
 
 def get_harry_answer(query: str, thread_id: str):
-    config = {"configurable": {"thread_id": thread_id}}
-    output = app.invoke({"messages": query}, config)
-    return output["messages"][-1].content
+
+    with MongoDBSaver.from_conn_info(
+         url = settings.MONGODB_URL, db_name="checkpoints"
+    ) as checkpointer:
+        graph = create_graph(checkpointer=checkpointer)
+        config = {"configurable": {"thread_id": thread_id}}
+        res = graph.invoke({"messages": [("human", query)]}, config)
+
+    return res["messages"][-1].content
 
 
-# async with AsyncMongoDBSaver.from_conn_info(
-#     host="localhost", port=27017, db_name="checkpoints"
-# ) as checkpointer:
-#     graph = create_react_agent(model, tools=tools, checkpointer=checkpointer)
-#     config = {"configurable": {"thread_id": "2"}}
-#     res = await graph.ainvoke(
-#         {"messages": [("human", "what's the weather in nyc")]}, config
-#     )
 
-#     latest_checkpoint = await checkpointer.aget(config)
-#     latest_checkpoint_tuple = await checkpointer.aget_tuple(config)
-#     checkpoint_tuples = [c async for c in checkpointer.alist(config)]
 
 
